ab019fe40e0670592c69161bb9677fe5c4ed2853
Cildn <linusdaniel458@gmail.com>
2025-05-12 22:00:55 +0100

    Final updates before PC shutdown

---
+++ prisma/migrations/20250512205900_shutdown/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "SiteConfig" ALTER COLUMN "heroVideo" SET DEFAULT 'hero.mp4',
+ALTER COLUMN "monthlyTarget" SET DEFAULT 100000,
+ALTER COLUMN "serviceFee" SET DEFAULT 100;
