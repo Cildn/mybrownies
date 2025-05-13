b87deef756c3b2057cf933827dcf3bbe6f07ece6
Cildn <98998632+Cildn@users.noreply.github.com>
2025-05-13 07:58:47 +0100

    Update apolloClient.ts

--- src/lib/graphql/apolloClient.ts
+++ src/lib/graphql/apolloClient.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, HttpLink, ApolloLink } from "@apollo/clien
 import { onError } from "@apollo/client/link/error";
 
 const httpLink = new HttpLink({
-  uri: process.env.NEXT_PUBLIC_GRAPHQL_URL || "http://localhost:4000/graphql",
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_URL
 });
 
 const authLink = new ApolloLink((operation, forward) => {
